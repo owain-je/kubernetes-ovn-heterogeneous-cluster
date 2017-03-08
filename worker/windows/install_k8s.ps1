70f122bb352251ec7da23c4047da4f2f1172cd22
Paulo Pires <pjpires@gmail.com>
2017-03-08 08:16:47 +0000

    Register kubelet as a service, disabled for now.

--- worker/windows/install_k8s.ps1
+++ worker/windows/install_k8s.ps1
@@ -1,5 +1,9 @@
 $K8S_PATH="C:\kubernetes"
 $K8S_VERSION="1.5.3"
+$HOSTNAME = hostname
+$K8S_MASTER_IP = "10.142.0.2"
+$K8S_DNS_SERVICE_IP = "10.100.0.10"
+$K8S_DNS_DOMAIN = "cluster.local"
 
 mkdir $K8S_PATH
 cd $K8S_PATH
@@ -17,4 +21,7 @@ mv kubernetes\node\bin\*.exe .
 Remove-Item -Recurse -Force kubernetes
 Remove-Item -Recurse -Force kubernetes-node-windows-amd64*
 
-# TODO register kube-proxy as a service and start it
+# TODO
+# Register kubelet as a service and start it
+#setx -m CONTAINER_NETWORK "external"
+#cmd /c 'sc create kubelet binPath= "\"C:\Program Files (x86)\Open vSwitch\bin\servicewrapper.exe\" kubelet \"C:\kubernetes\kubelet.exe\" -v=3 --hostname-override=sig-windows-worker-windows --cluster_dns=10.100.0.10 --cluster_domain=cluster.local --pod-infra-container-image=\"apprenda/pause\" --resolv-conf=\"\" --api_servers=\"http://10.142.0.2:8080\" --log-dir=\"C:\kubernetes\"" type= own start= auto error= ignore displayname= "Kubernetes Kubelet" obj= LocalSystem'
