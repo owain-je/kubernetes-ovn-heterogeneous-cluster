40634890e51cb11824d3b6b3db2b81876de1c620
Paulo Pires <pjpires@gmail.com>
2017-03-09 09:54:27 +0000

    Fix kubelet DNS flags.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

--- worker/windows/install_k8s.ps1
+++ worker/windows/install_k8s.ps1
@@ -24,4 +24,4 @@ Remove-Item -Recurse -Force kubernetes-node-windows-amd64*
 # TODO
 # Register kubelet as a service and start it
 #setx -m CONTAINER_NETWORK "external"
-#cmd /c 'sc create kubelet binPath= "\"C:\Program Files (x86)\Open vSwitch\bin\servicewrapper.exe\" kubelet \"C:\kubernetes\kubelet.exe\" -v=3 --hostname-override=sig-windows-worker-windows --cluster_dns=10.100.0.10 --cluster_domain=cluster.local --pod-infra-container-image=\"apprenda/pause\" --resolv-conf=\"\" --api_servers=\"http://10.142.0.2:8080\" --log-dir=\"C:\kubernetes\"" type= own start= auto error= ignore displayname= "Kubernetes Kubelet" obj= LocalSystem'
+#cmd /c 'sc create kubelet binPath= "\"C:\Program Files (x86)\Open vSwitch\bin\servicewrapper.exe\" kubelet \"C:\kubernetes\kubelet.exe\" -v=3 --hostname-override=sig-windows-worker-windows --cluster-dns=10.100.0.10 --cluster-domain=cluster.local --pod-infra-container-image=\"apprenda/pause\" --resolv-conf=\"\" --api_servers=\"http://10.142.0.2:8080\" --log-dir=\"C:\kubernetes\"" type= own start= auto error= ignore displayname= "Kubernetes Kubelet" obj= LocalSystem'
